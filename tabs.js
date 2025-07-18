7a2849204fc405438e4479aae972019493daa73f
Mildly Savage Digital LLC <veganlife_13@yahoo.com>
2025-07-18 11:09:36 -0400

    Delete tabs.js

--- tabs.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".tab").forEach(button => {
-  button.addEventListener("click", () => {
-    const tabName = button.getAttribute("data-tab");
-    document.querySelectorAll(".tab-content").forEach(section => {
-      section.style.display = "none";
-    });
-    document.getElementById(tabName).style.display = "block";
-  });
-});
